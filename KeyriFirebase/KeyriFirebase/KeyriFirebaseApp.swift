3412ab31f6164ed1e1e89a3482c8299ca7ecd694
Aditya Malladi <aditya@keyri.co>
2022-06-24 11:59:20 -0400

    init

---
+++ KeyriFirebase/KeyriFirebase/KeyriFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyriFirebaseApp.swift
+//  KeyriFirebase
+//
+//  Created by Aditya Malladi on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct KeyriFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
